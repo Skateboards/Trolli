705b246048316a2578529bcaf520f32c7132dfb9
Gurgen Meliksetyants <gurgen.meliksetyants@gmail.com>
2019-02-09 14:59:24 -0800

    Made an Api for users( controller and service)

---
+++ Sabio.Web/Controllers/Troll_UsersApiController.cs
@@ -0,0 +1,23 @@
+﻿using Sabio.Web.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sabio.Web.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/users")]
+    public class Troll_UsersApiController : ApiController
+    {
+        private readonly Troll_UsersService _service;
+
+        public Troll_UsersApiController(Troll_UsersService service)
+        {
+            _service = service;
+        }
+
+    }
+}
