ddc101c708f551f50122879c97fc1426d7e99cac
daniel tsang <danieltsang15@gmail.com>
2019-02-09 15:59:25 -0800

    finished working on users delete and select by id

--- Sabio.Web/Controllers/Api/Troll_UsersApiController.cs
+++ Sabio.Web/Controllers/Api/Troll_UsersApiController.cs
@@ -1,4 +1,6 @@
-﻿using Sabio.Web.Service;
+﻿using Sabio.Models.Domain;
+using Sabio.Models.Responses;
+using Sabio.Web.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,5 +21,22 @@ namespace Sabio.Web.Controllers
             _service = service;
         }
 
+        [Route("{id:int}"), HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            ItemResponse<TrolliUsers> responseBody = new ItemResponse<TrolliUsers>();
+            responseBody.Item = _service.GetById(id);
+            return Request.CreateResponse(HttpStatusCode.Created, responseBody);
+        }
+
+        [Route("{id:int}"), HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            ItemResponse<bool> response = new ItemResponse<bool>();
+            _service.Delete(id);
+            response.Item = true;
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
     }
 }
--- Sabio.Web/Service/Troll_UsersService.cs
+++ Sabio.Web/Service/Troll_UsersService.cs
@@ -1,6 +1,10 @@
-﻿using Sabio.Data.Providers;
+﻿using Sabio.Data;
+using Sabio.Data.Providers;
+using Sabio.Models.Domain;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,7 +17,49 @@ namespace Sabio.Web.Service
         public Troll_UsersService(IDataProvider dataProvider)
         {
             _dataProvider = dataProvider;
+
+        }
+
+        public TrolliUsers GetById(int Id)
+        {
+
+            TrolliUsers trolliData = null;
+            string procName = "[dbo].[Troll_Users_SelectById]";
+            _dataProvider.ExecuteCmd(procName
+            , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+            {
+                paramCollection.AddWithValue("@Id", Id);
+
+            }
+           , singleRecordMapper: delegate (IDataReader reader, short set)
+           {
+               trolliData = new TrolliUsers();
+
+               int startingIndex = 0;
+               trolliData.Id = reader.GetSafeInt32(startingIndex++);
+               trolliData.Password = reader.GetSafeString(startingIndex++);
+               trolliData.Username = reader.GetSafeString(startingIndex++);
+
+
+
+           });
+
+            return trolliData;
+
+        }
+
+        public void Delete(int Id)
+        {
+            string storeProc = "[dbo].[Trolli_User_Delete]";
+
+            _dataProvider.ExecuteNonQuery(storeProc,
+                delegate (SqlParameterCollection sqlParams)
+                {
+                    sqlParams.AddWithValue("@id", Id);
+                });
+
         }
 
     }
+
 }
