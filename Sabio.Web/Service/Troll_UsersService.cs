7e60dfa5c0b731ecab4a7c8fa85f335fa9a04faa
Gurgen Meliksetyants <gurgen.meliksetyants@gmail.com>
2019-02-09 15:16:59 -0800

    Made an Api for users( controller and service)

--- Sabio.Web/Service/Troll_UsersService.cs
+++ Sabio.Web/Service/Troll_UsersService.cs
@@ -1,6 +1,8 @@
 ﻿using Sabio.Data.Providers;
+using Sabio.Models.Requests;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +15,29 @@ namespace Sabio.Web.Service
         public Troll_UsersService(IDataProvider dataProvider)
         {
             _dataProvider = dataProvider;
+        }
+        public int Insert(Troll_UserAddRequest model)
+        {
+            int id = 0;
+
+            string procName = "[dbo].[Trolli_User_Insert]";
+
+            _dataProvider.ExecuteNonQuery(procName
+                , inputParamMapper: delegate (SqlParameterCollection sqlParams)
+                {
+                    sqlParams.AddWithValue("@DingCategory", model.Password);
+                    sqlParams.AddWithValue("@Value", model.UserName);
+
+                    SqlParameter idParameter = new SqlParameter("@Id", System.Data.SqlDbType.Int);
+                    idParameter.Direction = System.Data.ParameterDirection.Output;
+
+                    sqlParams.Add(idParameter);
+                }, returnParameters: delegate (SqlParameterCollection sqlParams)
+                {
+                    Int32.TryParse(sqlParams["@Id"].Value.ToString(), out id);
+                }
+                );
+            return id;
         }
 
     }
