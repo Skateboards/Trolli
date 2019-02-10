fa679216e53ea92827740ebbf0e6048e92f29b67
charlie.collier14 <charlie.collier14@gmail.com>
2019-02-09 16:55:58 -0800

    getting changes from master

---
+++ trolli.web.client/src/Services/mapServices.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+const key = "AIzaSyC54wON_jHY4WTYo__qxvRwOdd-aYoV1_s";
+const routeAPI = "https://maps.googleapis.com/maps/api/directions/json?";
+const geoLocationAPI =
+  "https://maps.googleapis.com/maps/api/geocode/json?address=";
+
+const getRoute = (originLatLong, destinationLatLong) => {
+  const config = {
+    method: "GET",
+    url:
+      routeAPI +
+      "origin=" +
+      { originLatLong } +
+      "&" +
+      "destination" +
+      { destinationLatLong } +
+      "mode=transit&transit_mode=bus&key=" +
+      { key },
+    withCredentials: true,
+    crossDomain: true,
+    headers: { "Content-Type": "application/json" }
+  };
+
+  return axios(config)
+    .then(global.onGlobalSuccess)
+    .catch(global.onGlobalError);
+};
+
+const geoLocation = (mapImageURL, onSuccess, onError) => {
+  const config = {
+    method: "GET",
+    url: geoLocationAPI + mapImageURL + "&key=" + key,
+    withCredentials: true,
+    crossDomain: true,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  return axios(config)
+    .then(onSuccess)
+    .catch(onError);
+};
+
+export { getRoute, geoLocation };
