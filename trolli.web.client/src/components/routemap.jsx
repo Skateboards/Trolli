1fd0c4cf6ae1409ca9d5755608a8ce1241e913c1
charlie.collier14 <charlie.collier14@gmail.com>
2019-02-10 11:57:49 -0800

    hapopy birthday will

--- trolli.web.client/src/components/routemap.jsx
+++ trolli.web.client/src/components/routemap.jsx
@@ -45,50 +45,11 @@ class RouteMap extends React.Component {
     let latlong = dest.results[0].geometry.location;
     this.setState(
       {
-        destination: latlong
+        destination: latlong,
+        renderMap: true
       },
-      () =>
-        mapServices.getRoute(
-          this.state.origin,
-          latlong,
-          this.getRouteSuccess,
-          this.getRouteFail
-        )
+      console.log(this.state)
     );
-  };
-
-  getRouteSuccess = response => {
-    let route = JSON.parse(response.data.item);
-    console.log(route);
-    const routeIds = [];
-    debugger;
-    for (let i = 0; i < route.routes[0].legs[0].steps.length; i++) {
-      const step = route.routes[0].legs[0].steps[i];
-      if ("travel_mode" in step && step.travel_mode === "TRANSIT") {
-        if (step.transit_details.line.short_name !== undefined) {
-          let routeId = step.transit_details.line.short_name;
-          routeIds.push(parseInt(routeId));
-        }
-      }
-    }
-    console.log(routeIds);
-    let payload = { Date: new Date(), routeId: routeIds };
-    mapServices.listOfDings(payload, this.dingSuccess, this.dingError);
-    this.setState({
-      renderMap: true
-    });
-  };
-
-  dingSuccess = res => {
-    console.log(res);
-  };
-
-  dingError = err => {
-    console.log(err);
-  };
-
-  getRouteFail = error => {
-    console.log(error);
   };
 
   geoLocationFail = error => {
@@ -97,9 +58,10 @@ class RouteMap extends React.Component {
 
   render() {
     return (
-      <div className="container text-center p-3 mt-3">
+      <div>
         <form>
           <div>
+            <label>Destination:</label>
             <input
               type="text"
               value={this.state.destination}
@@ -108,20 +70,15 @@ class RouteMap extends React.Component {
               className="form-control"
             />
           </div>
-
-          <div
-            className="text-center p-3 mt-3"
-            style={{
-              backgroundColor: "rgb(23, 162, 184)",
-              color: "white",
-              margin: "0 20px 20px 20px",
-              borderRadius: "50px"
-            }}
-            onClick={e => this.encodeAddress(e)}
-          >
-            <i className="fas fa-map-marked-alt mr-2" />
-            <strong>Get My Route</strong>
-          </div>
+          <p>
+            <button
+              className={"btn btn-success btn-block"}
+              type="button"
+              onClick={e => this.encodeAddress(e)}
+            >
+              Get my Route
+            </button>
+          </p>
         </form>
         {this.state.renderMap && <MapWithADirectionsRenderer {...this.state} />}
       </div>
