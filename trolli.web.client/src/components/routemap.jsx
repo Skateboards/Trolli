e4c931aac901573edf6d1e7ddffdc03ea67b8331
charlie.collier14 <charlie.collier14@gmail.com>
2019-02-10 12:02:43 -0800

    not RouteMap

--- trolli.web.client/src/components/routemap.jsx
+++ trolli.web.client/src/components/routemap.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as mapServices from "../Services/mapServices";
 import MapWithADirectionsRenderer from "./MapRender";
-import DingDisplaySmall from "../components/Dings/DingDisplaySmall";
 
 class RouteMap extends React.Component {
   constructor(props) {
@@ -9,11 +8,10 @@ class RouteMap extends React.Component {
     this.state = {
       origin: { lat: 0, lng: 0 },
       destination: "",
-      inputDestination: "",
       renderMap: false
     };
   }
-
+  //
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(response => {
       const origin = {
@@ -32,7 +30,7 @@ class RouteMap extends React.Component {
 
   encodeAddress = () => {
     let mapImageURL = encodeURIComponent(
-      this.state.inputDestination.replace(/[&\\#,+()$~%.'":*?<>{}]/g, "")
+      this.state.destination.replace(/[&\\#,+()$~%.'":*?<>{}]/g, "")
     );
     mapServices.geoLocation(
       mapImageURL,
@@ -47,92 +45,42 @@ class RouteMap extends React.Component {
     let latlong = dest.results[0].geometry.location;
     this.setState(
       {
-        destination: latlong
+        destination: latlong,
+        renderMap: true
       },
-      () =>
-        mapServices.getRoute(
-          this.state.origin,
-          latlong,
-          this.getRouteSuccess,
-          this.getRouteFail
-        )
+      console.log(this.state)
     );
-  };
-
-  getRouteSuccess = response => {
-    let route = JSON.parse(response.data.item);
-    console.log(route);
-    const routeIds = [];
-    debugger;
-    for (let i = 0; i < route.routes[0].legs[0].steps.length; i++) {
-      const step = route.routes[0].legs[0].steps[i];
-      if ("travel_mode" in step && step.travel_mode === "TRANSIT") {
-        if (step.transit_details.line.short_name !== undefined) {
-          let routeId = step.transit_details.line.short_name;
-          routeIds.push(parseInt(routeId));
-        }
-      }
-    }
-    console.log(routeIds);
-    let payload = { Date: new Date(), routeId: routeIds };
-    mapServices.listOfDings(payload, this.dingSuccess, this.dingError);
-  };
-
-  dingSuccess = res => {
-    console.log(res.data.items);
-    this.setState({
-      dings: res.data.items,
-      renderMap: true
-    });
-  };
-
-  dingError = err => {
-    console.log(err);
-  };
-
-  getRouteFail = error => {
-    console.log(error);
   };
 
   geoLocationFail = error => {
     console.log(error);
   };
 
-  mapMyDings = (ding, index) => {
-    return <DingDisplaySmall ding={ding} key={index} id={ding.dingId} />;
-  };
-
   render() {
     return (
-      <div className="container text-center p-3 mt-3">
+      <div>
         <form>
           <div>
+            <label>Destination:</label>
             <input
               type="text"
-              value={this.state.inputDestination}
+              value={this.state.destination}
               onChange={event => this.handleChange(event)}
-              name="inputDestination"
+              name="destination"
               className="form-control"
             />
           </div>
-
-          <div
-            className="text-center p-3 mt-3"
-            style={{
-              backgroundColor: "rgb(23, 162, 184)",
-              color: "white",
-              margin: "0 20px 20px 20px",
-              borderRadius: "50px"
-            }}
-            onClick={e => this.encodeAddress(e)}
-          >
-            <i className="fas fa-map-marked-alt mr-2" />
-            <strong>Get My Route</strong>
-          </div>
+          <p>
+            <button
+              className={"btn btn-success btn-block"}
+              type="button"
+              onClick={e => this.encodeAddress(e)}
+            >
+              Get my Route
+            </button>
+          </p>
         </form>
         {this.state.renderMap && <MapWithADirectionsRenderer {...this.state} />}
-
-        {this.state.renderMap && this.state.dings.map(this.mapMyDings)}
       </div>
     );
   }
