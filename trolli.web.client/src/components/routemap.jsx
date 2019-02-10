f5b680acb81126d8d44409a180266dfb2a0ab05f
charlie.collier14 <charlie.collier14@gmail.com>
2019-02-10 09:31:14 -0800

    getting routes from google response object

--- trolli.web.client/src/components/routemap.jsx
+++ trolli.web.client/src/components/routemap.jsx
@@ -45,11 +45,39 @@ class RouteMap extends React.Component {
     let latlong = dest.results[0].geometry.location;
     this.setState(
       {
-        destination: latlong,
-        renderMap: true
+        destination: latlong
       },
-      console.log(this.state)
+      () =>
+        mapServices.getRoute(
+          this.state.origin,
+          latlong,
+          this.getRouteSuccess,
+          this.getRouteFail
+        )
     );
+  };
+
+  getRouteSuccess = response => {
+    let route = JSON.parse(response.data.item);
+    console.log(route);
+    const routeIds = [];
+    debugger;
+    for (let i = 0; i < route.routes[0].legs[0].steps.length; i++) {
+      const step = route.routes[0].legs[0].steps[i];
+      if ("travel_mode" in step && step.travel_mode === "TRANSIT") {
+        if (step.transit_details.line.short_name !== undefined) {
+          routeIds.push(step.transit_details.line.short_name);
+        }
+      }
+    }
+    console.log(routeIds);
+    this.setState({
+      renderMap: true
+    });
+  };
+
+  getRouteFail = error => {
+    console.log(error);
   };
 
   geoLocationFail = error => {
@@ -58,10 +86,9 @@ class RouteMap extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container text-center p-3 mt-3">
         <form>
           <div>
-            <label>Destination:</label>
             <input
               type="text"
               value={this.state.destination}
@@ -71,13 +98,19 @@ class RouteMap extends React.Component {
             />
           </div>
           <p>
-            <button
-              className={"btn btn-success btn-block"}
-              type="button"
+            <div
+              className="text-center p-3 mt-3"
+              style={{
+                backgroundColor: "rgb(23, 162, 184)",
+                color: "white",
+                margin: "0 20px 20px 20px",
+                borderRadius: "50px"
+              }}
               onClick={e => this.encodeAddress(e)}
             >
-              Get my Route
-            </button>
+              <i className="fas fa-map-marked-alt mr-2" />
+              <strong>Get My Route</strong>
+            </div>
           </p>
         </form>
         {this.state.renderMap && <MapWithADirectionsRenderer {...this.state} />}
